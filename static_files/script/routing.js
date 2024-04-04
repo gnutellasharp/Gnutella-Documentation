e821d0d254af0e47b3d90a97bb1426d2ea22a0f1
michiel-schoofs <36503058+michiel-schoofs@users.noreply.github.com>
2024-04-04 16:51:27 +0200

    Added html routing

--- static_files/script/routing.js
+++ static_files/script/routing.js
@@ -1,8 +1,29 @@
 document.addEventListener("DOMContentLoaded",(_)=>{
-    pdfButtonInitialization();
+    ViewerInitialization();
 })
 
-function pdfButtonInitialization(){
+function ViewerInitialization(){
+    const searchParams = new URLSearchParams(window.location.search);
+    var html_view = searchParams.get("html_view") 
+    if(html_view!== null){
+        fetch(html_view).then(x=>x.text()).then(res=> {
+            const container = document.getElementById("viewer");
+            const shadowRoot = container.attachShadow({ mode: "open" });
+            shadowRoot.innerHTML =res;
+        });
+    }else{
+        var isLocalhost = window.location.host.includes("127.0.0.1") || window.location.host.includes("localhost");
+        var path = isLocalhost === true? "main.html" : "Gnutella-Documentation/main.html" 
+
+        fetch(path).then(x=>x.text()).then(res=> {
+            const container = document.getElementById("viewer");
+            container.innerHTML =res;
+            PdfButtonInitialization();
+        });
+    }
+}
+
+function PdfButtonInitialization(){
     document.querySelectorAll("[data-pdf]").forEach(btn=>{
         var linkToPdf = btn.dataset.pdf;
         if(linkToPdf !== '' && linkToPdf !== null){
